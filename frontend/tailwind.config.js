26c40ebe821acd421de172e9e84d9a379d449a4f
Priyanshu Shaw <124159556+priyaanshu101@users.noreply.github.com>
2025-07-16 14:12:17 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html", // Scans your main HTML file
+    "./src/**/*.{js,ts,jsx,tsx}", // Scans all JS, TS, JSX, TSX files in src/ for Tailwind classes
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
